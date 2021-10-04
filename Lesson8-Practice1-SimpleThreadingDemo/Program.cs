48f090875a69122f390c0f80ed0755ebf75bc55a
Владислав Куманов <edu.kumanov@outlook.com>
2021-10-04 04:33:40 +0300

    Add project files.

---
+++ Lesson8-Practice1-SimpleThreadingDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Lesson8_Practice1_SimpleThreadingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadStart starter = new ThreadStart(Counting);
+            Thread first = new Thread(starter);
+            Thread second = new Thread(starter);
+            first.Start(); second.Start();
+            first.Join(); second.Join();
+        }
+
+        static void Counting()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Count: {0} - Thread' {1}", i, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
